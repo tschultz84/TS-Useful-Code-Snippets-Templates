7172e07cfeccb893ca1a8004ad74f5f7fafd1740
alyosha_84 <77863290+tschultz84@users.noreply.github.com>
2023-03-17 07:15:15 -0600

    incremental work on CRUD

--- flask_examples/main.py
+++ flask_examples/main.py
@@ -2,6 +2,9 @@
 
 from flask import request, render_template, Blueprint
 import time
+import pandas as pd
+import json
+import os
 
 main = Blueprint('main',__name__)
 
@@ -57,3 +60,30 @@ def js_add_form_row():
     else: python_data['output_string'] = "You haven't yet posted an HTML variable."
 
     return render_template('js_add_form_row.html',python_data=python_data)
+
+    
+
+# Functional Flask / CRUD application writing to/from a CSV file.
+@main.route('/jquery_crud', methods=["GET","POST"])
+def jquery_crud():
+    
+    # Initialize.
+    python_data=dict()
+    python_data['output_string'] = "Viewing jquery_crud route from Python Flask."
+
+    # Retrieve tabular data, then convert it into JSON.
+    # These following lines of code are replaced in real life, for example if you retrieve from PostGreSQL, you need to re-write this code to serialize into JSON.
+    absolute_path = os.path.dirname(__file__)
+    file_path = os.path.join(absolute_path,"static/recipes.csv")
+    tabular_df = pd.read_csv(file_path)
+    tabular_json = tabular_df.to_json()
+
+    # Serialize into JSON.
+    python_data['json_out'] = json.dumps(tabular_json)
+
+    # Pull the data.
+    valuedict = request.args
+
+    print(f"Value of POST variables are {valuedict}!")
+
+    return render_template('jquery_crud.html',python_data=python_data)
